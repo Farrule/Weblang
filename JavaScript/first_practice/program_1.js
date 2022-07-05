0878c06e54c620e41c1899c76a5d8d90291059ae
Farrule <tsukasap0921@gmail.com>
2022-07-05 15:31:03 +0900

    first commit

---
+++ JavaScript/first_practice/program_1.js
@@ -0,0 +1,11 @@
+var obj = {
+    name: 'Carrot',
+    _for: 'Max',
+    details: {
+        color: 'orange',
+        size: 12
+    }
+};
+
+console.log(obj.details.color);
+console.log(obj['details']['size']);
